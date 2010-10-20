2bf07fd8df7117b0120de9eab3b39fc56dc4d8df
AurelienRibon <AurelienRibon@localhost>
2010-10-20 14:43:03 +0200

    * Tweaked the ParseCommanline method

--- AurelienRibon.Ui.Terminal/TerminalUtils.cs
+++ AurelienRibon.Ui.Terminal/TerminalUtils.cs
@@ -6,15 +6,21 @@ using System.Text.RegularExpressions;
 
 namespace AurelienRibon.Ui.Terminal {
 	public static class TerminalUtils {
+		/// <summary>
+		/// Parses a full command line and returns a Command object
+		/// containing the command name as well as the different arguments.
+		/// </summary>
+		/// <param name="line"></param>
+		/// <returns></returns>
 		public static Command ParseCommandLine(string line) {
 			string command = "";
 			List<string> args = new List<string>();
 
-			Match m = Regex.Match(line.Trim() + " ", @"^(.+?\s+|.+?$)(.*)");
+			Match m = Regex.Match(line.Trim() + " ", @"^(.+?)(?:\s+|$)(.*)");
 			if (m.Success) {
 				command = m.Groups[1].Value.Trim();
 				string argsLine = m.Groups[2].Value.Trim();
-				Match m2 = Regex.Match(argsLine + " ", @""".*?""\s+|.+?\s+");
+				Match m2 = Regex.Match(argsLine + " ", @"(?<!\\)"".*?(?<!\\)""|[\S]+");
 				while (m2.Success) {
 					string arg = Regex.Replace(m2.Value.Trim(), @"^""(.*?)""$", "$1");
 					args.Add(arg);
