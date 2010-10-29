414ed200f08115021dfc6a760bfb7949ad7ab4fd
AurelienRibon <AurelienRibon@localhost>
2010-10-29 15:26:11 +0200

    ! Made the input validation more robust ! Cleaned the project, commented

--- AurelienRibon.Ui.Terminal/Terminal.cs
+++ AurelienRibon.Ui.Terminal/Terminal.cs
@@ -9,6 +9,8 @@ using System.Media;
 
 namespace AurelienRibon.Ui.Terminal {
 	public class Terminal : TextBox {
+		protected enum CommandHistoryDirection { BACKWARD, FORWARD }
+
 		public List<string> RegisteredCommands { get; private set; }
 		public List<Command> CommandLog { get; private set; }
 		public bool IsPromptInsertedAtLaunch { get; set; }
@@ -32,12 +34,15 @@ namespace AurelienRibon.Ui.Terminal {
 			Prompt = "> ";
 			IsInputEnabled = false;
 
-			PreviewKeyDown += new KeyEventHandler(OnPreviewKeyDown);
 			Loaded += (s, e) => {
 				if (IsPromptInsertedAtLaunch)
 					InsertNewPrompt();
 			};
 		}
+
+		// --------------------------------------------------------------------
+		// PUBLIC INTERFACE
+		// --------------------------------------------------------------------
 
 		public void InsertNewPrompt() {
 			if (Text.Length > 0)
@@ -53,85 +58,141 @@ namespace AurelienRibon.Ui.Terminal {
 		}
 
 		// --------------------------------------------------------------------
-		// EVENT HANDLERS
+		// EVENT HANDLER
 		// --------------------------------------------------------------------
 
-		private void OnPreviewKeyDown(object sender, KeyEventArgs e) {
-			Terminal term = (Terminal)sender;
-			int initialLength = term.Text.Length;
+		protected override void OnPreviewKeyDown(KeyEventArgs e) {
+			// Store the length of Text before any input processing.
+			int initialLength = Text.Length;
 
+			// If input is not allowed, warn the user and discard its input.
 			if (!IsInputEnabled) {
-				e.Handled = true;
 				if (IsSystemBeepEnabled)
 					SystemSounds.Beep.Play();
+				e.Handled = true;
 				return;
 			}
 
-			if (CaretIndex < LastPomptIndex
-				|| (CaretIndex == LastPomptIndex && e.Key == Key.Back)
-				|| (CaretIndex == LastPomptIndex && e.Key == Key.Left)) {
+			// Test the caret position.
+			//
+			// 1. If located before the last prompt index
+			//    ==> Warn, set the caret at the end of input text, discard the input.
+			//
+			// 2. If located at the last prompt index and user tries to erase text
+			//    ==> Warn, discard the input.
+			//
+			// 3. If located at the last prompt index and user tries to move backward
+			//    ==> Warn, discard the input.
+			//
+			// 4. If located after (>=) the last prompt index and user presses the UP key
+			//    ==> Launch command history backward, discard the input.
+			//
+			// 5. If located after (>=) the last prompt index and user presses the UP key
+			//    ==> Launch command history forward, discard the input.
+			//
+			if (CaretIndex < LastPomptIndex) {
+				if (IsSystemBeepEnabled)
+					SystemSounds.Beep.Play();
 				CaretIndex = Text.Length;
 				e.Handled = true;
-				SystemSounds.Beep.Play();
-				return;
+			} else if (CaretIndex == LastPomptIndex && e.Key == Key.Back) {
+				if (IsSystemBeepEnabled)
+					SystemSounds.Beep.Play();
+				e.Handled = true;
+			} else if (CaretIndex == LastPomptIndex && e.Key == Key.Left) {
+				if (IsSystemBeepEnabled)
+					SystemSounds.Beep.Play();
+				e.Handled = true;
 			} else if (CaretIndex >= LastPomptIndex && e.Key == Key.Up) {
-				if (indexInLog > 0)
-					indexInLog--;
-				if (CommandLog.Count > 0) {
-					Text = GetTextWithPromptSuffix(CommandLog[indexInLog].Raw);
-					CaretIndex = Text.Length;
-				}
+				HandleCommandHistoryRequest(CommandHistoryDirection.BACKWARD);
 				e.Handled = true;
 			} else if (CaretIndex >= LastPomptIndex && e.Key == Key.Down) {
-				if (indexInLog < CommandLog.Count - 1)
-					indexInLog++;
-				if (CommandLog.Count > 0) {
-					Text = GetTextWithPromptSuffix(CommandLog[indexInLog].Raw);
-					CaretIndex = Text.Length;
-				}
+				HandleCommandHistoryRequest(CommandHistoryDirection.FORWARD);
 				e.Handled = true;
 			}
 
-			// Push en ENTER key
-			if (e.Key == Key.Enter) {
-				string line = Text.Substring(LastPomptIndex);
-				Text += "\n";
-				IsInputEnabled = false;
-				LastPomptIndex = int.MaxValue;
-
-				Command cmd = TerminalUtils.ParseCommandLine(line);
-				CommandLog.Add(cmd);
-				indexInLog = CommandLog.Count;
-				RaiseCommandEntered(cmd);
-				e.Handled = true;
-
-				// Push on TAB key
-			} else if (e.Key == Key.Tab) {
-				if (CaretIndex != Text.Length) {
-					e.Handled = true;
-					return;
+			// If input has not yet been discarded, test the key for special inputs.
+			// ENTER  => validates the input
+			// TAB    => launches command completion with registered commands
+			if (!e.Handled) {
+				switch (e.Key) {
+					case Key.Enter:
+						HandleEnterKey();
+						e.Handled = true;
+						break;
+					case Key.Tab:
+						HandleTabKey();
+						e.Handled = true;
+						break;
 				}
+			}
 
-				string line = Text.Substring(LastPomptIndex);
-				string[] commands = GetAssociatedCommands(line);
+			base.OnPreviewKeyDown(e);
+		}
 
-				if (commands.Length > 0) {
-					if (CaretIndex > LastPomptIndex)
-						Text = Text.Remove(LastPomptIndex);
-					Text += GetCommonPrefix(commands);
-					CaretIndex = Text.Length;
-				}
+		// --------------------------------------------------------------------
+		// VIRTUAL METHODS
+		// --------------------------------------------------------------------
 
-				e.Handled = true;
+		protected virtual void HandleCommandHistoryRequest(CommandHistoryDirection direction) {
+			switch (direction) {
+				case CommandHistoryDirection.BACKWARD:
+					if (indexInLog > 0)
+						indexInLog--;
+					if (CommandLog.Count > 0) {
+						Text = GetTextWithPromptSuffix(CommandLog[indexInLog].Raw);
+						CaretIndex = Text.Length;
+					}
+					break;
+
+				case CommandHistoryDirection.FORWARD:
+					if (indexInLog < CommandLog.Count - 1)
+						indexInLog++;
+					if (CommandLog.Count > 0) {
+						Text = GetTextWithPromptSuffix(CommandLog[indexInLog].Raw);
+						CaretIndex = Text.Length;
+					}
+					break;
 			}
 		}
 
-		private string GetTextWithPromptSuffix(string suffix) {
+		protected virtual void HandleEnterKey() {
+			string line = Text.Substring(LastPomptIndex);
+			Text += "\n";
+			IsInputEnabled = false;
+			LastPomptIndex = int.MaxValue;
+
+			Command cmd = TerminalUtils.ParseCommandLine(line);
+			CommandLog.Add(cmd);
+			indexInLog = CommandLog.Count;
+			RaiseCommandEntered(cmd);
+		}
+
+		protected virtual void HandleTabKey() {
+			if (CaretIndex != Text.Length)
+				return;
+
+			string line = Text.Substring(LastPomptIndex);
+			string[] commands = GetAssociatedCommands(line);
+
+			if (commands.Length > 0) {
+				if (CaretIndex > LastPomptIndex)
+					Text = Text.Remove(LastPomptIndex);
+				Text += GetCommonPrefix(commands);
+				CaretIndex = Text.Length;
+			}
+		}
+
+		// --------------------------------------------------------------------
+		// CLASS SPECIFIC UTILITIES
+		// --------------------------------------------------------------------
+
+		protected string GetTextWithPromptSuffix(string suffix) {
 			string ret = Text.Substring(0, LastPomptIndex);
 			return ret + suffix;
 		}
 
-		private string[] GetAssociatedCommands(string prefix) {
+		protected string[] GetAssociatedCommands(string prefix) {
 			List<string> ret = new List<string>();
 			foreach (var cmd in RegisteredCommands)
 				if (cmd.StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
@@ -139,14 +200,18 @@ namespace AurelienRibon.Ui.Terminal {
 			return ret.ToArray();
 		}
 
-		private string GetShortestString(string[] strs) {
+		// --------------------------------------------------------------------
+		// GENERAL UTILITIES
+		// --------------------------------------------------------------------
+
+		protected string GetShortestString(string[] strs) {
 			string ret = strs[0];
 			foreach (string str in strs)
 				ret = str.Length < ret.Length ? str : ret;
 			return ret;
 		}
 
-		private string GetCommonPrefix(string[] strs) {
+		protected string GetCommonPrefix(string[] strs) {
 			string shortestStr = GetShortestString(strs);
 			for (int i = 0; i < shortestStr.Length; i++)
 				foreach (string str in strs)
